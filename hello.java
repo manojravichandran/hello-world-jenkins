a6f209d199bc8f66af2d75dd46394a95702e259a
manojravichandran <manojravichandran18@gmail.com>
2019-03-28 11:20:38 +0530

    Update hello.java
    .

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 public static void main(String args[]){
 	System.out.println("hello Jenkins");
-	System.out.println("This is trigger built acc to commit");
+	System.out.println("This is trigger built acc to commitsss");
 }
 }
