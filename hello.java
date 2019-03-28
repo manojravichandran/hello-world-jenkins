4440ff126372f56ed27bafe4c9c5e14720fa87dd
manojravichandran <manojravichandran18@gmail.com>
2019-03-28 11:18:07 +0530

    Update hello.java
    jenkins trigger

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 public static void main(String args[]){
-	System.out.println("hello world");
-	
+	System.out.println("hello Jenkins");
+	System.out.println("This is trigger built acc to commit");
 }
 }
