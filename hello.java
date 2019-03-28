6ba01c78cfa067f16ac42f63d14ef9fc0bee975b
manojravichandran <manojravichandran18@gmail.com>
2019-03-28 11:35:47 +0530

    Update hello.java
    works good

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello{
 public static void main(String args[]){
 	System.out.println("hello Jenkins");
 	System.out.println("This is trigger built acc to commits");
-		System.out.println("Works fine");
+		System.out.println("Works Good");
 }
 }
